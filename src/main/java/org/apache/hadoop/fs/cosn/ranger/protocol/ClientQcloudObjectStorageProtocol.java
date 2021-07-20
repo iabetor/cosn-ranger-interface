ec47beddf90e49ede4d58e5509b3620c6b75fff6
vinsonruan <vinsonruan@tencent.com>
2021-07-20 11:34:01 +0800

    extract class from other project

---
+++ src/main/java/org/apache/hadoop/fs/cosn/ranger/protocol/ClientQcloudObjectStorageProtocol.java
@@ -0,0 +1,69 @@
+package org.apache.hadoop.fs.cosn.ranger.protocol;
+
+import org.apache.hadoop.fs.cosn.ranger.security.authorization.PermissionRequest;
+import org.apache.hadoop.fs.cosn.ranger.security.authorization.PermissionResponse;
+import org.apache.hadoop.fs.cosn.ranger.security.sts.GetSTSRequest;
+import org.apache.hadoop.fs.cosn.ranger.security.sts.GetSTSResponse;
+import org.apache.hadoop.fs.cosn.ranger.security.token.DelegationTokenIdentifier;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.retry.Idempotent;
+import org.apache.hadoop.security.token.Token;
+
+import java.io.IOException;
+
+public interface ClientQcloudObjectStorageProtocol {
+    /**
+     * Get a valid Delegation Token.
+     *
+     * @param renewer the designated renewer for the token
+     * @throws IOException
+     */
+    @Idempotent
+    Token<DelegationTokenIdentifier> getDelegationToken(Text renewer) throws IOException;
+
+    /**
+     * Renew an existing delegation token.
+     *
+     * @param token delegation token obtained earlier
+     * @return the new expiration time
+     * @throws IOException
+     */
+    @Idempotent
+    long renewDelegationToken(Token<DelegationTokenIdentifier> token) throws IOException;
+
+    /**
+     * Cancel an existing delegation token.
+     *
+     * @param token delegation token
+     * @throws IOException
+     */
+    @Idempotent
+    void cancelDelegationToken(Token<DelegationTokenIdentifier> token) throws IOException;
+
+    /**
+     * Check Permission.
+     *
+     * @param request permission request
+     * @return permission response, include allowed flag and real user name.
+     * @throws IOException
+     */
+    @Idempotent
+    PermissionResponse checkPermission(PermissionRequest request) throws IOException;
+
+    /**
+     * Check Permission.
+     *
+     * @param request get sts request
+     * @return get sts response
+     * @throws IOException
+     */
+    @Idempotent
+    GetSTSResponse getSTS(GetSTSRequest request) throws IOException;
+
+
+    @Idempotent
+    String getRangerPolicyUrl() throws IOException;
+
+    @Idempotent
+    String getExtralAuthInfo()throws IOException;
+}
